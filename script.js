8f2c0c039c9b87a8fb39ef4a5233a3afc41ecea0
AkhilaNegi <122104289+AkhilaNegi@users.noreply.github.com>
2023-03-28 12:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+      const form = document.querySelector('form');
+      const nameInput = document.querySelector('#name');
+      const yearInput = document.querySelector('#year');
+      const url = document.querySelector('#url');
+
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const name = nameInput.value;
+        const year = yearInput.value;
+        const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+        url.textContent = `https://localhost:8080/${queryString}`;
+      });
+    
